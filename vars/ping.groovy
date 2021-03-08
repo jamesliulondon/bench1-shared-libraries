cc50b79fe7bcd068fa6d7494c565ff3350a66d52
James Liu <jamesliulondon@gmail.comQ>
2021-03-08 16:54:52 +0000

    initial x

--- vars/ping.groovy
+++ vars/ping.groovy
@@ -1,26 +1,6 @@
-import sun.awt.X11.XAnyEvent
-import sun.util.resources.cldr.nyn.CalendarData_nyn_UG
+#!/usr/bin/env groovy
 
 def call() {
-    pipeline {
-        agent any
-        stages {
-            stage('ping') {
-                step {
-                    echo 'ping'
-                }
-                step {
-                    echo 'ping'
-                }
-            }
-            stage('joy') {
-                step {
                     echo 'jeremiah'
-                }
-                step {
                     echo 'was a bullfrog'
-                }
-            }
-        }
-    }
 }
